ca43e310e4738d8cc009a16d39ef5dd756ecb039
Naim <naim.baijid@gmail.com>
2026-09-03 23:54:58 +0600

    update imports

--- chatbot.py
+++ chatbot.py
@@ -13,7 +13,7 @@ model = ChatGoogleGenerativeAI(
 )
 
 parser = PydanticOutputParser(pydantic_object=Query)
-prompt = PromptTemplate(template=task_classifier_prompt, input_variables=["query"], partial_variables={
+prompt = PromptTemplate(template=task_classifier_prompt_template , input_variables=["query"], partial_variables={
     "format_instructions": parser.get_format_instructions()
 })
 
@@ -23,14 +23,13 @@ classifier_chain = prompt | model | parser
 string_parser = StrOutputParser()
 
 # prompt templates for different tasks
-all_recommendation_prompt = PromptTemplate(template=all_recommendation_prompt, input_variables=["query"])
-food_recommendation_prompt = PromptTemplate(template=food_recommendation_prompt, input_variables=["query"])
-spot_recommendation_prompt = PromptTemplate(template=spot_recommendation_prompt, input_variables=["query"])
+food_recommendation_prompt = PromptTemplate(template=food_recommendation_prompt_template , input_variables=["query"])
+spot_recommendation_prompt = PromptTemplate(template=spot_recommendation_prompt_template , input_variables=["query"])
+budget_recommendation_prompt = PromptTemplate(template=budget_recommendation_prompt_template , input_variables=["query"])
 budget_recommendation_prompt = PromptTemplate(template=budget_recommendation_prompt, input_variables=["query"])
 
 
 # chains for different tasks
-all_chain = all_recommendation_prompt | model | string_parser
 food_chain = food_recommendation_prompt | model | string_parser
 spot_chain = spot_recommendation_prompt | model | string_parser
 budget_chain = budget_recommendation_prompt | model | string_parser
