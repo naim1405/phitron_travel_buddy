106b8ec9c32ecf7e3fa56715d8f73251e0ba4d4b
Naim <naim.baijid@gmail.com>
2026-09-04 01:28:55 +0600

    use langchain HumanMessage & AIMessage

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from models import Message
 from chatbot import ask_ai
+from langchain.messages import HumanMessage, AIMessage
 
 
 if "messages" not in st.session_state:
@@ -30,23 +31,22 @@ for item in st.session_state.chat_history:
 
 # conversation 
 for item in st.session_state.messages:
-    with st.chat_message(item.role):
+    role = "assistant" if isinstance(item, AIMessage) else "user"
+    with st.chat_message(role):
         st.markdown(item.content)
 
 
 if prompt := st.chat_input("Ask me anything"):
     # display user msg and add to chat history
-    usr_msg = Message(role="user", content=prompt)
-    st.session_state.messages.append(usr_msg)
-    st.session_state.chat_history.append(prompt)
-    with st.chat_message(usr_msg.role):
-        st.markdown(usr_msg.content)
+    st.session_state.chat_history.append(HumanMessage(content=prompt).content)
+    with st.chat_message("user"):
+        st.markdown(prompt)
     st.sidebar.header(prompt, divider="orange")
 
     # generate assistant response and add to chat history
-    ai_response = ask_ai(prompt)
-    assistant_msg = Message(role="assistant", content=ai_response)
-    st.session_state.messages.append(assistant_msg)
-    with st.chat_message(assistant_msg.role):
-        st.markdown(assistant_msg.content)
+    ai_response = ask_ai(prompt, st.session_state.messages)
+    st.session_state.messages.append(HumanMessage(content=prompt))
+    st.session_state.messages.append(AIMessage(content=ai_response))
+    with st.chat_message("assistant"):
+        st.markdown(ai_response)
 
